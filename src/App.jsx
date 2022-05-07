9d99258e1bdf9dc7f260a0ec4589517ebc64dcc4
hervis30 <hervisjonat@gmail.com>
2022-05-07 17:18:33 -0500

    se importan componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Header/>
-      <Main/>
+      <Header />
+      <Main />
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import '../App.css';
 
 export const Header = () => {
   return (
     <div className='headerForm'>
-        <div className='imgForm'>
-            <h1>img</h1>
-        </div>
-        
-        <div>
-            <h1>srghrth</h1>
-        </div>
+      <div className='imgForm'>
+        <h1>img</h1>
+      </div>
+
+      <div>
+        <h1>srghrth</h1>
+      </div>
     </div>
   )
 }
